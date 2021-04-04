7a8aadaa2cafb241db6fac0afa1dd60f72faf43b
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-04-04 12:39:04 +0430

    Start shopping

--- Projects/ShoppingDataBase/3.sql
+++ Projects/ShoppingDataBase/3.sql
@@ -1,1 +1,20 @@
+create table user
+(
+	id char(5),
+    name varchar(20) not null,
+    cellphone char(11),
+    referred_by char(5) unique,
+    credit int,
+    primary key (ID)
+);
 
+create table driver
+(
+	id char(5),
+    name varchar(20) not null,
+    cellphone char(11),
+    address varchar(50),
+    is_active int,
+    score int,
+    check (is_active in (0,1))
+);
