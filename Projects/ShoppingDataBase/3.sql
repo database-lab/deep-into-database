46f26a453b3e6fb9bfc115aae2dc4b8b69d962f5
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-04-04 12:47:43 +0430

    new tables added

--- Projects/ShoppingDataBase/3.sql
+++ Projects/ShoppingDataBase/3.sql
@@ -1,3 +1,5 @@
+SET sql_mode='ANSI_QUOTES';
+
 create table user
 (
 	id char(5),
@@ -16,5 +18,24 @@ create table driver
     address varchar(50),
     is_active int,
     score int,
-    check (is_active in (0,1))
+    check (is_active in (0,1)),
+    primary key (id)
+);
+
+create table orders
+(
+	id char(5),
+    user_id char(5),
+    driver_id char(5) default null,
+    estimated_value int,
+    price int,
+    is_round_trip int,
+    start_time date,
+    finish_time date,
+    score int,
+    check (is_round_trip in (0,1)),
+    check (score > 0 and score < 6),
+    primary key (id),
+    foreign key (user_id) references user(id),
+    foreign key (driver_id) references driver(id)
 );
