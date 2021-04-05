98b8231187f96654ce7a301adb8b2a2b6705a228
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-04-05 21:35:51 +0430

    Files added

--- Projects/ShoppingDataBase/test.sql
+++ Projects/ShoppingDataBase/test.sql
@@ -1,11 +1,51 @@
-select *
-from user;
+-- A success
+update user
+	set name = "farbod" 
+    where id = "00001";
 
-select *
-from orders;
+-- B success
+select name
+from user
+where user.id not in (  select user_id
+						from orders    );
 
-select *
-from payment;
+-- C success
+delete from user
+where user.id not in (  select user_id
+						from orders    );
 
-select *
-from driver;
+-- D success
+select id
+from user
+where name = "farbod";
+
+-- E success
+select cellphone
+from user
+where user.cellphone in (   select cellphone
+							from driver      );
+                            
+-- F faild
+
+-- G success
+select id
+from orders
+where datediff(start_time, finish_time) >= 1;
+
+-- H faild
+
+-- I success
+select avg( datediff(start_time, finish_time) ) as avg
+from orders
+where score > 3;
+
+-- J faild
+
+-- K faild
+
+-- L faild
+
+-- M success
+select sum(price) as total
+from order natural join user
+where referred_by = "farbod";
